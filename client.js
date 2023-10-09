73e3a36225f0bfd87fd49fc32ee78dc349c44e50
jdvgsa <johnr.dagsa@outlook.com>
2023-10-09 02:35:00 -0600

    halfway point

--- client.js
+++ client.js
@@ -53,3 +53,33 @@ function increaseCount(){
 function decreaseCount(){
   count--;
 }
+
+/* misunderstood first part of assignment where instructions said
+ "increment or decrement a number using ++/--;" 
+I thought I would showcase this part
+in HTML by making a ++/--; button again. will include button made
+with the top part of code for the assignment anyway, figuring it couldn't hurt */
+
+/* convert a number to a string with Number.toString(); */
+
+let myNum = 10;
+let increasedNum = myNum + 1;
+// console.log ("increased number:", increasedNum);
+// increased number: 11
+let decreasedNum = myNum - 1;
+// console.log("decreased number:", decreasedNum);
+// decreased number: 9
+
+document.querySelector("#ogNum0").innerText = myNum;
+document.querySelector("#ogNum1").innerText = increasedNum;
+document.querySelector("#ogNum2").innerText = decreasedNum;
+
+/* convert number (integer to a string with number.toString();) */
+
+
+let numString = myNum.toString();
+// console.log("num to string:", numString);
+// num to string: 10
+// '10' to 10. unsure if you want values should be different.
+document.querySelector("#stringNum").innerText = numString;
+
