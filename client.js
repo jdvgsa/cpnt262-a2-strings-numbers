8d518a3c21e9303e6c4ab7f4e162985902338b17
jdvgsa <johnr.dagsa@outlook.com>
2023-10-09 01:00:32 -0600

    cleaned up comments in JS file

--- client.js
+++ client.js
@@ -4,13 +4,13 @@ const increaseBy1 = document.getElementById("myButton");
 const decreaseBy1 = document.getElementById("myButton2");
 
 /* ******************************************************************
-Solution 1 to increase and decrease buttons, behaves 
-the same way as the eventlistener function.
+Solution 1 to increase and decrease buttons, 
+behaves the same way as the eventlistener function.
 ****************************************************************** */
 // var count = 0;
 // increaseBy1.onclick = function() {
 //   count++;
-//   // document.getElementById("counter").innerHTML = count;
+//   document.getElementById("counter").innerHTML = count;
 //   showCount.innerHTML = count;
 // };
 
@@ -23,16 +23,25 @@ the same way as the eventlistener function.
 var count = 0;
 
 increaseBy1.addEventListener("click", function() {
-  // count++;
+  /* count++; would work here but 
+     chose to use a function (line 49) for practice */
   increaseCount();
-  // document.getElementById("counter").innerHTML = count;
+  /* ** document.getElementById("counter").innerHTML = count; **
+     if used, will have the same result with syntax below
+     opted for shorter variation code for practice.
+  */
   showCount.innerHTML = count;
 });
 
 decreaseBy1.addEventListener("click", function() {
-  // count--;
+  /* count--; , would work here but 
+     chose to use a function (line 49) for practice */
   decreaseCount();
-  // document.getElementById("counter").innerHTML = count;
+
+  /* *********************************************
+  ** document.getElementById("counter").innerHTML = count; **
+  ** does the same as | showCount.innerHTML = count; **
+  ********************************************* */ 
   showCount.innerHTML = count;
 });
 
